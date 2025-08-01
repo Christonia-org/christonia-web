0e2653b9defb5cb00c094370f17dcfa24203381f
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-08-01 21:44:53 +0300

    Mount the how it works section in the landing page

--- christonia/src/app/page.tsx
+++ christonia/src/app/page.tsx
@@ -1,10 +1,12 @@
 import JourneySection from "@/components/JourneySection/JourneySection";
+import HowItWorksSection from "@/components/HowItWorksSection/HowItWorksSection";
 import SupportUsSection from "@/components/SupportUsSection/SupportUsSection";
 
 export default function Home() {
   return (
     <>
       <JourneySection />
+      <HowItWorksSection />
       <SupportUsSection />
     </>
   );
