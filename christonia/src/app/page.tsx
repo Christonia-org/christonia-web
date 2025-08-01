05fd70986df248b8ff0f6b3dcf086fa20c97a8f9
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-08-01 21:00:13 +0300

    Build the journey section

--- christonia/src/app/page.tsx
+++ christonia/src/app/page.tsx
@@ -1,8 +1,10 @@
+import JourneySection from "@/components/JourneySection/JourneySection";
 import SupportUsSection from "@/components/SupportUsSection/SupportUsSection";
 
 export default function Home() {
   return (
     <>
+      <JourneySection />
       <SupportUsSection />
     </>
   );
