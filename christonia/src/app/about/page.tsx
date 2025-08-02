0651fa6b1156b62bccad439c25ebaab4a4082599
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-08-02 12:53:50 +0300

    Build and mount the overview section

--- christonia/src/app/about/page.tsx
+++ christonia/src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import AboutSection from "./components/AboutSection/AboutSection";
+import OverviewSection from "./components/OverviewSection/OverviewSection";
 
 export default function AboutPage() {
   return (
     <>
         <AboutSection />
+        <OverviewSection />
     </>
   );
 }
