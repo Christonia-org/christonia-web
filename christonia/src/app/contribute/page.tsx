afa9f80a2ae60eb2af428adf2c622e299ebd8f83
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-08-04 19:56:39 +0300

    Build and style the why you should contribute section

--- christonia/src/app/contribute/page.tsx
+++ christonia/src/app/contribute/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import HowToContributeSection from "./components/HowToContributeSection/HowToContributeSection";
+import WhyYouShouldContributeSection from "./components/WhyYouShouldContributeSection/WhyYouShouldContributeSection";
 
 export default function ContributePage() {
     return (
         <div className={styles.page}>
             <HowToContributeSection />
+            <WhyYouShouldContributeSection />
         </div>
     );
 }
