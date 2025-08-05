b25ab3f9eeef7eb71d1f67dec5106b580bdf22f7
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-08-05 15:11:47 +0300

    Build and add the to our contributors section to the team page

--- christonia/src/app/team/page.tsx
+++ christonia/src/app/team/page.tsx
@@ -1,12 +1,14 @@
 import styles from './page.module.css';
 import MeetTheTeamSection from './components/MeetTheTeamSection/MeetTheTeamSection';
 import SupportUsSection from '@/components/SupportUsSection/SupportUsSection';
+import ToOurContributorsSection from './components/ToOurContributorsSection/ToOurContributorsSection';
 
 export default function TeamPage() {
     return (
         <>
             <div className={styles.teamPage}>
                 <MeetTheTeamSection />
+                <ToOurContributorsSection />
             </div>
             <SupportUsSection />
         </>
