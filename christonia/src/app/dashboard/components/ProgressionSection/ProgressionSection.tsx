ca84293076932ca6e99cbe258eda191d214ef1fa
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-08-09 13:28:18 +0300

    Build the progression section

--- christonia/src/app/dashboard/components/ProgressionSection/ProgressionSection.tsx
+++ christonia/src/app/dashboard/components/ProgressionSection/ProgressionSection.tsx
@@ -9,13 +9,35 @@ export default function ProgressionSection() {
                 <CourseCard
                     courseName="Introduction to Web Development"
                     courseTrack="Front-End Fundamentals"
-                    courseImageLink="/moon.svg"
+                    courseImageLink="/moon.svg" // You might want to update this image path
                     lessonsCompleted={11}
                     totalLessons={20}
                     blogsPublished={1}
                     totalBlogs={3}
                     quizScore={49}
                     totalQuizScore={60}
+                />
+                <CourseCard
+                    courseName="Advanced JavaScript Concepts"
+                    courseTrack="Front-End Mastery"
+                    courseImageLink="/moon.svg" // Dummy image path
+                    lessonsCompleted={8}
+                    totalLessons={15}
+                    blogsPublished={2}
+                    totalBlogs={4}
+                    quizScore={75}
+                    totalQuizScore={80}
+                />
+                <CourseCard
+                    courseName="Data Science with Python"
+                    courseTrack="Back-End Development"
+                    courseImageLink="/moon.svg" // Dummy image path
+                    lessonsCompleted={25}
+                    totalLessons={25} // This card will show 100% completion
+                    blogsPublished={5}
+                    totalBlogs={5}
+                    quizScore={90}
+                    totalQuizScore={90}
                 />
             </div>
         </section>
