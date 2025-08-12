d62997d2a2a45163899241ccbcf596cac55b4cb6
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-08-12 15:58:35 +0300

    Update header and journey section links

--- christonia/src/components/SignedInHeader/SignedInHeader.tsx
+++ christonia/src/components/SignedInHeader/SignedInHeader.tsx
@@ -1,36 +1,64 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import styles from "./SignedInHeader.module.css";
 import Image from "next/image";
 
-export default function SignedInHeader({ userProfilePicUrl }: SignedInHeaderProps) {
+export default function SignedInHeader({
+  userProfilePicUrl,
+}: SignedInHeaderProps) {
   return (
     <header className={styles.header}>
-      <div className={styles.logoSection}> 
-        <img src="/icon.png" alt="Christonia Logo" className={styles.logo} /> 
-        <h1 className={styles.title}>Christonia</h1> 
+      <div className={styles.logoSection}>
+        <img src="/icon.png" alt="Christonia Logo" className={styles.logo} />
+        <h1 className={styles.title}>Christonia</h1>
       </div>
       <nav>
         <ul className={styles.navList}>
-            <li><Link href={"/dashboard"} className={styles.navLink}>Dashboard</Link></li> 
-            <li><Link href={"/all-paths"} className={styles.navLink}>All Paths</Link></li> 
-            <li><Link href={"/about"} className={styles.navLink}>About</Link></li>
-            <li><Link href={"/glossary"} className={styles.navLink}>Glossary</Link></li>
-            <li><Link href={"/support-us"} className={styles.navLink}>Support us</Link></li>
-            <li><Link href={"/blogs"} className={styles.navLink}>Blogs</Link></li>
-            <li>
-                <Link href={"/profile/user-info"} className={styles.profilePhoto}>
-                  <Image 
-                    src={userProfilePicUrl || "/profile-icon.svg"}
-                    alt="Profile Image" 
-                    width={50}
-                    height={50}
-                    className={styles.profileImage} 
-                    onError={(e) => { e.currentTarget.src = '/profile-icon.svg'; }}
-                  />
-                </Link>
-            </li>
+          <li>
+            <Link href={"/dashboard"} className={styles.navLink}>
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link href={"/paths"} className={styles.navLink}>
+              All Paths
+            </Link>
+          </li>
+          <li>
+            <Link href={"/about"} className={styles.navLink}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href={"/glossary"} className={styles.navLink}>
+              Glossary
+            </Link>
+          </li>
+          <li>
+            <Link href={"/support-us"} className={styles.navLink}>
+              Support us
+            </Link>
+          </li>
+          <li>
+            <Link href={"/blogs"} className={styles.navLink}>
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <Link href={"/profile/user-info"} className={styles.profilePhoto}>
+              <Image
+                src={userProfilePicUrl || "/profile-icon.svg"}
+                alt="Profile Image"
+                width={50}
+                height={50}
+                className={styles.profileImage}
+                onError={(e) => {
+                  e.currentTarget.src = "/profile-icon.svg";
+                }}
+              />
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
--- christonia/src/components/SignedOutHeader/SignedOutHeader.tsx
+++ christonia/src/components/SignedOutHeader/SignedOutHeader.tsx
@@ -4,18 +4,42 @@ import styles from "./SignedOutHeader.module.css";
 export default function SignedOutHeader() {
   return (
     <header className={styles.header}>
-      <div className={styles.logoSection}> 
-        <img src="/icon.png" alt="Christonia Logo" className={styles.logo} /> 
-        <h1 className={styles.title}>Christonia</h1> 
+      <div className={styles.logoSection}>
+        <img src="/icon.png" alt="Christonia Logo" className={styles.logo} />
+        <h1 className={styles.title}>Christonia</h1>
       </div>
       <nav>
         <ul className={styles.navList}>
-            <li><Link href={"/all-paths"} className={styles.navLink}>All Paths</Link></li> 
-            <li><Link href={"/about"} className={styles.navLink}>About</Link></li>
-            <li><Link href={"/glossary"} className={styles.navLink}>Glossary</Link></li>
-            <li><Link href={"/support-us"} className={styles.navLink}>Support us</Link></li>
-            <li><Link href={"/sign-in"} className={styles.navLink}>Sign in</Link></li>
-            <li><Link href={"/sign-up"} className={styles.getStartedButton}>Get Started</Link></li>
+          <li>
+            <Link href={"/paths"} className={styles.navLink}>
+              All Paths
+            </Link>
+          </li>
+          <li>
+            <Link href={"/about"} className={styles.navLink}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href={"/glossary"} className={styles.navLink}>
+              Glossary
+            </Link>
+          </li>
+          <li>
+            <Link href={"/support-us"} className={styles.navLink}>
+              Support us
+            </Link>
+          </li>
+          <li>
+            <Link href={"/sign-in"} className={styles.navLink}>
+              Sign in
+            </Link>
+          </li>
+          <li>
+            <Link href={"/sign-up"} className={styles.getStartedButton}>
+              Get Started
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
