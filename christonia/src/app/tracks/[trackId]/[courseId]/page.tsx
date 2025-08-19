fad42ba1e9c2e8b6955079cb1aac1e51bff2ded6
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-08-19 17:19:06 +0300

    Style the course page

--- christonia/src/app/tracks/[trackId]/[courseId]/page.tsx
+++ christonia/src/app/tracks/[trackId]/[courseId]/page.tsx
@@ -2,6 +2,7 @@ import ModuleComponent from "./components/ModuleComponent/ModuleComponent";
 import { getCourseById } from "@/lib/mock/courses";
 import Image from "next/image";
 import SupportUsSection from "@/components/SupportUsSection/SupportUsSection";
+import styles from "./page.module.css";
 
 export default function CoursePage({ params }: CoursePageProps) {
   const { courseId } = params;
@@ -9,17 +10,17 @@ export default function CoursePage({ params }: CoursePageProps) {
   console.log(course);
   return (
     <>
-      <div>
+      <div className={styles.page}>
         <Image
           src={course.imageLink || "/default-course-logo.svg"}
           alt="Course Logo"
           width={100}
           height={100}
         />
-        <h1>{course.name}</h1>
-        <h2>Overview</h2>
-        <p>{course.description}</p>
-        <div>
+        <h1 className={styles.courseName}>{course.name}</h1>
+        <h2 className={styles.overviewTitle}>Overview</h2>
+        <p className={styles.overviewDescription}>{course.description}</p>
+        <div className={styles.modulesContainer}>
           {course.modules.map((module) => (
             <ModuleComponent key={module.id} module={module} />
           ))}
