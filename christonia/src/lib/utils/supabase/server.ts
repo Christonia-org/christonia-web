49ebbe049c573cf6a4d5d4e95fa23ec9619dbb34
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-09-02 12:31:07 +0300

    Test supabase to ensure it works

--- christonia/src/lib/utils/supabase/server.ts
+++ christonia/src/lib/utils/supabase/server.ts
@@ -4,19 +4,22 @@ import { cookies } from "next/headers";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const createClient = async (cookieStore: ReturnType<typeof cookies>) => {
-  const cookies = await cookieStore;
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(supabaseUrl!, supabaseKey!, {
     cookies: {
-      getAll() {
-        return cookies.getAll();
+      async getAll() {
+        return (await cookieStore).getAll();
       },
       setAll(cookiesToSet) {
         try {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookies.set(name, value, options)
+          cookiesToSet.forEach(async ({ name, value, options }) =>
+            (await cookieStore).set(name, value, options)
           );
-        } catch {}
+        } catch {
+          // The `setAll` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+        }
       },
     },
   });
