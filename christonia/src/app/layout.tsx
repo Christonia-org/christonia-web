1307c797315de859e4a7c101ec89bff43e2ce53b
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-12-17 11:38:08 +0200

    Make the app layout flex

--- christonia/src/app/layout.tsx
+++ christonia/src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header/Header";
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import Footer from "@/components/Footer/Footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Christonia",
@@ -21,11 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Header />
-        <main>
-          {children}
-        </main>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
