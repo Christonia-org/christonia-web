f80911652e2fc79c73ec763a071800fb66b779b8
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-12-17 15:49:27 +0200

    Migrate the contribute card to tw

--- christonia/src/app/contribute/components/HowToContributeSection.tsx
+++ christonia/src/app/contribute/components/HowToContributeSection.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import ContributeCard from "./ContributeCard/ContributeCard";
+import ContributeCard from "./ContributeCard";
 
 export default function HowToContributeSection() {
   return (
