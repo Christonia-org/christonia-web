328d8ab23b1bc53fb65c8118135857bc56594fb1
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-12-17 16:26:04 +0200

    Migrate the team page to tw

--- christonia/src/app/team/page.tsx
+++ christonia/src/app/team/page.tsx
@@ -1,12 +1,11 @@
-import styles from "./page.module.css";
-import MeetTheTeamSection from "./components/MeetTheTeamSection/MeetTheTeamSection";
-import SupportUsSection from "@/components/SupportUsSection";
+import MeetTheTeamSection from "./components/MeetTheTeamSection";
 import ToOurContributorsSection from "./components/ToOurContributorsSection/ToOurContributorsSection";
+import SupportUsSection from "@/components/SupportUsSection";
 
 export default function TeamPage() {
   return (
     <>
-      <div className={styles.teamPage}>
+      <div className="flex flex-col items-center gap-16 md:gap-24 bg-background min-h-screen w-full">
         <MeetTheTeamSection />
         <ToOurContributorsSection />
       </div>
