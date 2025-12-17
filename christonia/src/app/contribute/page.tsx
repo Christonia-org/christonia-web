6c8edfa2d88051ada2a4d55d9f544af205fbe495
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-12-17 15:45:03 +0200

    Migrate the contribute page to tw

--- christonia/src/app/contribute/page.tsx
+++ christonia/src/app/contribute/page.tsx
@@ -1,4 +1,3 @@
-import styles from "./page.module.css";
 import HowToContributeSection from "./components/HowToContributeSection/HowToContributeSection";
 import WhyYouShouldContributeSection from "./components/WhyYouShouldContributeSection/WhyYouShouldContributeSection";
 import SupportUsSection from "@/components/SupportUsSection";
@@ -6,7 +5,7 @@ import SupportUsSection from "@/components/SupportUsSection";
 export default function ContributePage() {
   return (
     <>
-      <div className={styles.page}>
+      <div className="flex flex-col items-center min-h-screen text-white bg-background pt-0">
         <HowToContributeSection />
         <WhyYouShouldContributeSection />
       </div>
