0abac2b6129520e128df2aca63fb274b347c6b7f
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-12-29 10:02:41 +0200

    Change lesson page interface to promise

--- christonia/src/app/tracks/[trackId]/[courseId]/lesson/[lessonId]/page.tsx
+++ christonia/src/app/tracks/[trackId]/[courseId]/lesson/[lessonId]/page.tsx
@@ -1,15 +1,16 @@
 import LessonViewer from "./components/LessonViewer";
 
 interface LessonsPageProps {
-  params: {
+  params: Promise<{
     trackId: string;
     courseId: string;
     lessonId: string;
-  };
+  }>;
 }
 
 export default async function LessonsPage({ params }: LessonsPageProps) {
   const { trackId, courseId, lessonId } = await params;
+
   return (
     <LessonViewer trackId={trackId} courseId={courseId} lessonId={lessonId} />
   );
