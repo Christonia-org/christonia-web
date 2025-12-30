352c862d25ed83e190671043c4cc1af520d2b061
Mr-MichaelK <michael1kolanjian2@gmail.com>
2025-12-30 13:49:08 +0200

    Create the supabase files

---
+++ christonia/src/lib/utils/supabase/server.ts
@@ -0,0 +1,29 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, // Use your Anon/Public key
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored because our middleware handles
+            // the session refresh.
+          }
+        },
+      },
+    }
+  );
+}
